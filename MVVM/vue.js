a2c63e57002e7331ecd4b77086cca9bf1618202f
WernerWu5 <1034320246@qq.com>
2020-10-16 16:34:49 +0800

    观察者 发布订阅

--- MVVM/vue.js
+++ MVVM/vue.js
@@ -27,6 +27,7 @@ function Vue(options = {}) {
 //  vue如何实现对 数组方法push等的变异 请查看 arr.js 文件
 function Observe(data) {
   Object.keys(data).forEach(key => {
+    let dep = new Dep()
     let val = data[key]
     observe(val) // 实现深度监听
     Object.defineProperty(data, key, {
@@ -34,6 +35,7 @@ function Observe(data) {
       enumerable: true, // 必须为true，否则后面无法枚举
       get() {
         console.log('获取', key, '值：', val)
+        Dep.target && dep.add(Dep.target)
         return val // 不能使用data[key]，否则会循环调用get方法
       },
       set(newVal) {
@@ -42,6 +44,7 @@ function Observe(data) {
           return
         }
         val = newVal
+        dep.notify(newVal)
         observe(val) // 判断newVal是否为对象
       }
     })
@@ -87,7 +90,9 @@ function Compile(el, vm) {
         arr.forEach(key => {
           val = val[key]
         })
-
+        new Watcher(vm, RegExp.$1, function (newVal) {
+          node.textContent = text.replace(reg, newVal).trim()
+        })
         // 将文本内容 替换
         node.textContent = text.replace(reg, val).trim()
       }
@@ -102,22 +107,67 @@ function Compile(el, vm) {
           // console.log('att.value', att.value)
           if (name.includes('v-model')) {
             node.value = vm[exp]
+
+            new Watcher(vm, RegExp.$1, function (newVal) {
+              node.value = text.replace(reg, newVal).trim()
+            })
+
+            node.addEventListener('input', function (e) {
+              let newVal = e.target.value
+              // console.log(newVal);
+              vm[exp] = newVal
+            })
           }
+
         })
       }
 
       if (node.childNodes && node.childNodes.length) {
         replace(node)
       }
-      
     })
   }
 
   replace(fragment)
+
   // 将虚拟DOM重新导入到真实DOM中
   vm._el.appendChild(fragment)
 }
 
+// 添加发布订阅
 function Dep() {
+  this.subs = []
+}
 
+Dep.prototype.add = function (sub) {
+  this.subs.push(sub)
+}
+
+Dep.prototype.notify = function () {
+  this.subs.forEach(sub => {
+    sub.update()
+  })
+}
+
+// 观察者
+function Watcher(vm, exp, fn) {
+  this.vm = vm
+  this.exp = exp
+  this.fn = fn
+  Dep.target = this
+  let val = vm
+  let arr = exp.split('.')
+  arr.forEach(exp => {
+    val = val[exp]
+  })
+  Dep.target = null
+}
+
+Watcher.prototype.update = function () {
+  let val = this.vm
+  let arr = this.exp.split('.')
+  arr.forEach(key => {
+    val = val[key]
+  })
+  this.fn(val)
 }
